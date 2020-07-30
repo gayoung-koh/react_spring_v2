cc12daedf6cb0aea7a428b825ce8f15dbe3ee484
gayoung-koh <gayoung.koh@hanwha-qcells.com>
2020-07-31 01:19:24 +0900

    first commit

---
+++ src/main/java/com/example/react_spring_v2/ReactSpringV2Application.java
@@ -0,0 +1,13 @@
+package com.example.react_spring_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/react_spring_v2/ReactSpringV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_spring_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
